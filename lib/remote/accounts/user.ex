e8078e6a52f99bd8b8f9b5e9e19217ea6dfe0f08
Frankline Okari <okarifrankline5678@gmail.com>
2022-04-27 09:37:34 +0300

    added: users table and schema file

---
+++ lib/remote/accounts/user.ex
@@ -0,0 +1,19 @@
+defmodule Remote.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :points, :integer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:points])
+    |> validate_required([:points])
+  end
+end
---
+++ test/support/fixtures/accounts_fixtures.ex
@@ -0,0 +1,20 @@
+defmodule Remote.AccountsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Remote.Accounts` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        points: 42
+      })
+      |> Remote.Accounts.create_user()
+
+    user
+  end
+end
