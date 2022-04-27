09563357c19b660d5436e8900a4b104bd6d8a684
Frankline Okari <okarifrankline5678@gmail.com>
2022-04-27 10:08:42 +0300

    updated: seeds.exs to add 1000000 users

--- lib/remote/accounts/user.ex
+++ lib/remote/accounts/user.ex
@@ -1,19 +1,32 @@
 defmodule Remote.Accounts.User do
+  @moduledoc """
+  Defines struct representing a user
+  """
+
   use Ecto.Schema
+
   import Ecto.Changeset
+
+  alias Ecto.Changeset
+
+  @type t :: %__MODULE__{}
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "users" do
-    field :points, :integer
+    field :points, :integer, default: 0
 
     timestamps()
   end
 
-  @doc false
-  def changeset(user, attrs) do
+  @doc """
+  Called during the creation and update of a user's points
+  """
+  @spec changeset(user :: t(), attrs :: map()) :: Changeset.t()
+  def changeset(user \\ %__MODULE__{}, attrs) do
     user
     |> cast(attrs, [:points])
     |> validate_required([:points])
+    |> validate_number(:points, greater_than_or_equal_to: 0, less_than_or_equal_to: 100)
   end
 end
